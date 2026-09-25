8cda1ffa23311ddabacbef4078077450bfe007b9
Rotkiw148 <Sawczuk.Wiktor@zst.radom.pl>
2026-09-25 13:08:41 +0200

    zad1

---
+++ script.js
@@ -0,0 +1,8 @@
+//zad1
+let a = parsefloat(prompt("podaj 1 liczbe"));
+let b = parsefloat(prompt("podaj 2 liczbe"));
+let c = parsefloat(prompt("podaj 3 liczbe"));
+
+let suma = a+b+c;
+
+document.writeln(`<p>suma ${a} +${b}+${c}=${suma}</p>`)
