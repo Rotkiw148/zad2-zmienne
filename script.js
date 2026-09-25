a21c62c3ce1e71b45b4a1c658161c7eecb2232c2
Rotkiw148 <Sawczuk.Wiktor@zst.radom.pl>
2026-09-25 13:11:45 +0200

    zad2

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ let c = parsefloat(prompt("podaj 3 liczbe"));
 
 let suma = a+b+c;
 
-document.writeln(`<p>suma ${a} +${b}+${c}=${suma}</p>`)
+document.writeln(`<p>suma ${a} +${b}+${c}=${suma}</p>`);
+
+//zad2
+let srednia = suma/3;
+document.writeln(`<p>średnia: ${srednia}</p>`);
+
